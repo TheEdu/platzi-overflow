25863d9f30c9f76a83c6225684eac07454087113
Eduardo Denis <edigde@gmail.com>
2018-12-22 00:42:53 -0300

    Se agregaron routas para la api de question.

--- server/app.js
+++ server/app.js
@@ -1,9 +1,9 @@
 import express from 'express'
+import { question } from './routes'
 
 const app = express()
 
-app.get('/', (req, res) => {
-    res.send("Hola desde Express!")
-})
+app.use('/api/questions', question)
+app.use('/', (req, res) => res.send("Hola Mundo!"))
 
 export default app
